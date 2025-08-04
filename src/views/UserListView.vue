9f31c29e5311b6b307c863324ff8121e75eeeb5a
Titouan Zickler <titouan.zickler@gmail.com>
2025-08-04 15:02:06 +0200

    Correction UserList

--- src/views/UserListView.vue
+++ src/views/UserListView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import { fetchUserOnlyList } from '../services/UserService'
+import { fetchUserOnlyList } from '../services/userService'
 import DashboardLayout from '../layouts/DashboardLayout.vue'
 
 const users = ref([])
